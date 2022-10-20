57163a2ea615771b60a547820b7a1e9664f96a72
c86ec23b-fef1-4979-b2fa-b9adc351b8cc <87239823+c86ec23b-fef1-4979-b2fa-b9adc351b8cc@users.noreply.github.com>
2022-10-20 15:49:21 +0300

    Update AutoTurret.ts

--- src/Entity/Tank/AutoTurret.ts
+++ src/Entity/Tank/AutoTurret.ts
@@ -18,6 +18,7 @@
 
 import ObjectEntity from "../Object";
 import Barrel from "./Barrel";
+import AbstractBoss from "../Boss/AbstractBoss";
 
 import { BarrelBase } from "./TankBody";
 import { Colors, MotionFlags, NametagFlags, ObjectFlags, Stat, StyleFlags } from "../../Const/Enums";
@@ -113,7 +114,7 @@ export default class AutoTurret extends ObjectEntity {
     }
 
     public tick(tick: number) {
-        this.physics.size = baseRatio * this.sizeFactor;
+        this.physics.size = this.owner instanceof AbstractBoss && this.owner.name.values.name === 'Defender'? 25 * this.owner.sizeFactor / 2 : baseRatio * this.sizeFactor; // lazy bad workaround, change soon hopefully
 
         this.ai.aimSpeed = this.turret.bulletAccel;
         // Top Speed
