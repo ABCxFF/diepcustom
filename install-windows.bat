1defc7840c5acf961261032f78d30e77545ec181
n0deee <diogodogogo@gmail.com>
2022-12-06 11:24:19 -0300

    Modified

--- install-windows.bat
+++ install-windows.bat
@@ -5,15 +5,22 @@ WHERE npm>nul || GOTO install_npm
 
 node autom/installer.js
 
-GOTO end
+GOTO end_succ
 
 :install_nodejs
 echo [ERROR] NODE.JS NOT FOUND. Please install it: https://nodejs.org/en/
-GOTO end
+GOTO end_err
 
 :install_npm
 echo [ERROR] NPM NOT FOUND. Please install it.
+GOTO end_err
+
+:end_err
+PAUSE
+GOTO end
+
+:end_succ
+TIMEOUT 15
 GOTO end
 
 :end
-TIMEOUT 15
--- run-windows.bat
+++ run-windows.bat
@@ -2,8 +2,8 @@
 : Check for node and npm installation
 (WHERE node>nul && WHERE npm>nul) || GOTO please_run_installer
 
+: Note that NPM will exit immediately if an error occurs.
 npm run server
-
 GOTO end
 
 :please_run_installer
